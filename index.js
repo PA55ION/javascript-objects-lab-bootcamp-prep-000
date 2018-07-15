a8c84f04289d8bfeaf57f7a299f2703d3af956dd
Phakarat Campbell <jirallc@gmail.com>
2018-07-15 17:35:31 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,24 @@
+var recipes = {};
+
+function updateObjectWithKeyAndValue(object, key, value) {
+  return Object.assign({}, object, {[key]: value})
+  return object;
+}
+
+// updateObjectWithKeyAndValue(object, 'prop2', 2);
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value
+  return object;
+
+}
+function deleteFromObjectByKey(object, key) {
+  delete object.key;
+  return deleteFromObjectByKey;
+}
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  Object.assign({}, object, {[key});
+    return destructivelyDeleteFromObjectByKey;
+
+}
